7bd9e95271bebf6af500863484f54137941dea3c
Nguyen Xuan Tien <nguyenxuantien0596@gmail.com>
2017-05-06 23:58:23 +0700

    MPU6050_AVR128_PC

---
+++ CODE_MPU6050_VS_PC/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace GiaoTiepMpu6050
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort = new System.IO.Ports.SerialPort(this.components);
+            this.ComBBaud = new System.Windows.Forms.ComboBox();
+            this.ComBPort = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.BTKetNoi = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.TexBTenfile = new System.Windows.Forms.TextBox();
+            this.BTStart = new System.Windows.Forms.Button();
+            this.ZedGraphGiaToc = new ZedGraph.ZedGraphControl();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.ComBchedo = new System.Windows.Forms.ComboBox();
+            this.BtLoad = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // serialPort
+            // 
+            this.serialPort.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.dataReceive);
+            // 
+            // ComBBaud
+            // 
+            this.ComBBaud.FormattingEnabled = true;
+            this.ComBBaud.Location = new System.Drawing.Point(74, 81);
+            this.ComBBaud.Name = "ComBBaud";
+            this.ComBBaud.Size = new System.Drawing.Size(137, 21);
+            this.ComBBaud.TabIndex = 11;
+            // 
+            // ComBPort
+            // 
+            this.ComBPort.FormattingEnabled = true;
+            this.ComBPort.Location = new System.Drawing.Point(74, 41);
+            this.ComBPort.Name = "ComBPort";
+            this.ComBPort.Size = new System.Drawing.Size(137, 21);
+            this.ComBPort.TabIndex = 12;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Baud rate";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Cổng ";
+            // 
+            // BTKetNoi
+            // 
+            this.BTKetNoi.Location = new System.Drawing.Point(74, 128);
+            this.BTKetNoi.Name = "BTKetNoi";
+            this.BTKetNoi.Size = new System.Drawing.Size(93, 27);
+            this.BTKetNoi.TabIndex = 8;
+            this.BTKetNoi.Text = "Kết Nối";
+            this.BTKetNoi.UseVisualStyleBackColor = true;
+            this.BTKetNoi.Click += new System.EventHandler(this.BTKetNoi_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.ComBBaud);
+            this.groupBox1.Controls.Add(this.ComBPort);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.BTKetNoi);
+            this.groupBox1.Location = new System.Drawing.Point(19, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(245, 178);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Cài đặt";
+            // 
+            // TexBTenfile
+            // 
+            this.TexBTenfile.Location = new System.Drawing.Point(74, 48);
+            this.TexBTenfile.Name = "TexBTenfile";
+            this.TexBTenfile.Size = new System.Drawing.Size(137, 20);
+            this.TexBTenfile.TabIndex = 14;
+            // 
+            // BTStart
+            // 
+            this.BTStart.Location = new System.Drawing.Point(133, 132);
+            this.BTStart.Name = "BTStart";
+            this.BTStart.Size = new System.Drawing.Size(93, 27);
+            this.BTStart.TabIndex = 8;
+            this.BTStart.Text = "Bắt đầu";
+            this.BTStart.UseVisualStyleBackColor = true;
+            this.BTStart.Click += new System.EventHandler(this.BTStart_Click);
+            // 
+            // ZedGraphGiaToc
+            // 
+            this.ZedGraphGiaToc.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ZedGraphGiaToc.BackColor = System.Drawing.SystemColors.Control;
+            this.ZedGraphGiaToc.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ZedGraphGiaToc.Location = new System.Drawing.Point(282, 21);
+            this.ZedGraphGiaToc.Name = "ZedGraphGiaToc";
+            this.ZedGraphGiaToc.ScrollGrace = 0D;
+            this.ZedGraphGiaToc.ScrollMaxX = 0D;
+            this.ZedGraphGiaToc.ScrollMaxY = 0D;
+            this.ZedGraphGiaToc.ScrollMaxY2 = 0D;
+            this.ZedGraphGiaToc.ScrollMinX = 0D;
+            this.ZedGraphGiaToc.ScrollMinY = 0D;
+            this.ZedGraphGiaToc.ScrollMinY2 = 0D;
+            this.ZedGraphGiaToc.Size = new System.Drawing.Size(739, 400);
+            this.ZedGraphGiaToc.TabIndex = 16;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label6);
+            this.groupBox3.Controls.Add(this.ComBchedo);
+            this.groupBox3.Controls.Add(this.BtLoad);
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Controls.Add(this.TexBTenfile);
+            this.groupBox3.Controls.Add(this.BTStart);
+            this.groupBox3.Location = new System.Drawing.Point(19, 224);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(245, 197);
+            this.groupBox3.TabIndex = 18;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "File";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(14, 77);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(42, 13);
+            this.label6.TabIndex = 21;
+            this.label6.Text = "Chế độ";
+            // 
+            // ComBchedo
+            // 
+            this.ComBchedo.FormattingEnabled = true;
+            this.ComBchedo.Location = new System.Drawing.Point(74, 74);
+            this.ComBchedo.Name = "ComBchedo";
+            this.ComBchedo.Size = new System.Drawing.Size(137, 21);
+            this.ComBchedo.TabIndex = 20;
+            // 
+            // BtLoad
+            // 
+            this.BtLoad.Location = new System.Drawing.Point(27, 132);
+            this.BtLoad.Name = "BtLoad";
+            this.BtLoad.Size = new System.Drawing.Size(90, 27);
+            this.BtLoad.TabIndex = 16;
+            this.BtLoad.Text = "Load File";
+            this.BtLoad.UseVisualStyleBackColor = true;
+            this.BtLoad.Click += new System.EventHandler(this.BtLoad_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(14, 51);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(45, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Tên File";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(1043, 446);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.ZedGraphGiaToc);
+            this.Controls.Add(this.groupBox1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Giao Tiếp MPU6050";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort;
+        private System.Windows.Forms.ComboBox ComBBaud;
+        private System.Windows.Forms.ComboBox ComBPort;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button BTKetNoi;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private ZedGraph.ZedGraphControl ZedGraphGiaToc;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button BTStart;
+        private System.Windows.Forms.TextBox TexBTenfile;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button BtLoad;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox ComBchedo;
+    }
+}
+
---
+++ CODE_MPU6050_VS_PC/Form1.cs
@@ -0,0 +1,391 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Ports;
+using ZedGraph;
+
+namespace GiaoTiepMpu6050
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int start_mode = 0; // cho phép ve do thi 
+        bool modefile = false;
+        bool Dothi = true;
+        private int millisecond=0; 
+        DateTime Datetime_Now =new DateTime();
+        //DateTime Datetime_Start = new DateTime();
+        //DateTime Datetime_Delay = new DateTime();
+        //TimeSpan Time_Delay = new TimeSpan();
+        RollingPointPairList List_Ax = new RollingPointPairList(60000);
+        RollingPointPairList List_Ay = new RollingPointPairList(60000);
+        RollingPointPairList List_Az = new RollingPointPairList(60000);
+        RollingPointPairList List_Roll_x = new RollingPointPairList(60000); //List_Roll_x List_Pitch_y  List_Yaw_z
+        RollingPointPairList List_Pitch_y = new RollingPointPairList(60000);
+        RollingPointPairList List_Yaw_z = new RollingPointPairList(60000);
+
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            TexBTenfile.Text = "test";
+            string[] portnames = SerialPort.GetPortNames();
+            int[] PortNumber = new int[portnames.Length];
+            for (int i = 0; i < portnames.Length; i++ )
+            {
+                PortNumber[i] = int.Parse(portnames[i].Substring(3));
+            }
+            Array.Sort(PortNumber);
+            foreach(int Number in PortNumber)
+            {
+                ComBPort.Items.Add("COM" + Number);
+            }
+            ComBBaud.Items.Add("9600");
+            ComBBaud.Items.Add("14400");
+            ComBBaud.Items.Add("19200");
+            ComBBaud.Items.Add("56000");
+            ComBBaud.Items.Add("115200");
+            ComBchedo.Items.Add("Chỉ vẽ đồ thị");
+            ComBchedo.Items.Add("Vẽ đồ thị và ghi file");
+            ComBchedo.SelectedItem = "Chỉ vẽ đồ thị";
+               // Array.Sort(portnames);
+            //ComBPort.Items.AddRange(portnames);
+            // Do Thi 
+            //ZedGraphGocQuay.GraphPane.Title.Text ="Đồ Thị Góc Quay";
+            //ZedGraphGocQuay.GraphPane.XAxis.Title.Text = "Thời Gian, giây";
+            //ZedGraphGocQuay.GraphPane.YAxis.Title.Text = "Góc Quay";
+            ZedGraphGiaToc.GraphPane.Title.Text = " Gia tốc, góc quay";
+            ZedGraphGiaToc.GraphPane.XAxis.Title.Text="Thời Gian (giây)";
+            ZedGraphGiaToc.GraphPane.YAxis.Title.Text = "Gia tốc (g), góc quay (Rad)";
+
+            LineItem Ax = ZedGraphGiaToc.GraphPane.AddCurve("Roll x", List_Ax, Color.Blue, SymbolType.None);
+            LineItem Ay = ZedGraphGiaToc.GraphPane.AddCurve("Pitch y", List_Ay, Color.Red, SymbolType.None);
+            LineItem Az = ZedGraphGiaToc.GraphPane.AddCurve("Yaw z", List_Az, Color.Yellow, SymbolType.None);
+            LineItem Roll_x = ZedGraphGiaToc.GraphPane.AddCurve("Ax", List_Roll_x, Color.Violet, SymbolType.None);
+            LineItem Pitch_y = ZedGraphGiaToc.GraphPane.AddCurve("Ay", List_Pitch_y, Color.Tomato, SymbolType.None);
+            LineItem Yaw_z = ZedGraphGiaToc.GraphPane.AddCurve("Az", List_Yaw_z, Color.Turquoise, SymbolType.None);
+
+            ZedGraphGiaToc.IsShowPointValues = false;
+            ZedGraphGiaToc.GraphPane.XAxis.Scale.Min = 0;
+            ZedGraphGiaToc.GraphPane.XAxis.Scale.Max = 10;
+            ZedGraphGiaToc.GraphPane.YAxis.Scale.Min = -3;
+            ZedGraphGiaToc.GraphPane.YAxis.Scale.Max = 3;
+            ZedGraphGiaToc.GraphPane.XAxis.Scale.MinorStep = 1;
+            //ZedGraphGiaToc.GraphPane.XAxis.Scale.MajorStep = 1;
+            ZedGraphGiaToc.AxisChange();
+            double k = -1.02345;
+            string t = "-1.02345";
+            double n=-0.22;
+            n=Convert.ToDouble(t);
+            //textBox1.Text = k.ToString();
+            //textBox2.Text = n.ToString();
+        }
+        
+
+       
+
+        private void BTKetNoi_Click(object sender, EventArgs e)
+        {
+            if(BTKetNoi.Text=="Kết Nối")
+            {
+                try
+                {
+                    serialPort.PortName = ComBPort.Text;
+                    serialPort.BaudRate = int.Parse(ComBBaud.Text);
+                    serialPort.Open();
+                    MessageBox.Show("Kết nối thành công !", "Thành công", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                    BTKetNoi.Text = "Ngắt Kết Nối";
+                }
+                catch
+                {
+                    MessageBox.Show("Kết nối thất bại !", "Thất bại", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+            } else if(BTKetNoi.Text=="Ngắt Kết Nối")
+            {
+                serialPort.Close();
+                BTKetNoi.Text = "Kết Nối";
+            }
+        }
+        private void BTStart_Click(object sender, EventArgs e)
+        {
+            if (BTStart.Text == "Bắt đầu")
+            {
+                
+                ZedGraphGiaToc.IsShowPointValues = false;
+                ClearZedGraph();
+                if (ComBchedo.SelectedItem.ToString() == "Chỉ vẽ đồ thị")
+                {
+                    modefile = false;
+                }
+                else modefile = true;
+                TexBTenfile.Enabled = false;
+                BtLoad.Enabled = false;
+                //timer1.Enabled = true;
+               // Datetime_Start=DateTime.Now
+                Datetime_Now = DateTime.Now ;
+                String filepath = System.IO.Directory.GetCurrentDirectory() + @"\data\"+TexBTenfile.Text+".txt";
+                FileStream fs = new FileStream(filepath, FileMode.Create);
+                StreamWriter sWriter = new StreamWriter(fs, Encoding.UTF8);
+                sWriter.WriteLine("MPU6050 - " + Datetime_Now.ToString());
+                sWriter.Flush();
+                //sWriter.WriteLine
+                fs.Close();
+                start_mode = 1;
+                BTStart.Text = "Dừng";
+                
+            }
+            else
+            {
+                //timer1.Enabled = false;
+                start_mode = 0;
+                
+                TexBTenfile.Enabled = true;
+                BtLoad.Enabled = true;
+                BTStart.Text = "Bắt đầu";
+                ZedGraphGiaToc.IsShowPointValues = true;
+                
+            }
+
+        }
+        
+
+        private void dataReceive(object sender, SerialDataReceivedEventArgs e)
+        {
+            if (start_mode == 1)
+            {
+                string s = "";
+                try
+                {
+                    s = serialPort.ReadTo("\r\n");//ReadExisting();
+                    double datet = ((TimeSpan)(DateTime.Now - Datetime_Now)).TotalMilliseconds; // tính khoảng thời gian
+                    datet = Math.Round(datet / 1000, 3);
+
+                    
+                    string[] araystring = s.Split(' ');
+                    draw(Convert.ToDouble(araystring[0]), Convert.ToDouble(araystring[1]), Convert.ToDouble(araystring[2]), Convert.ToDouble(araystring[3]), Convert.ToDouble(araystring[4]), Convert.ToDouble(araystring[5]), datet); // ve do thi 6 diem 
+                   
+                   if (modefile == true)
+                    {
+                        StreamWriter sWriter = new StreamWriter(System.IO.Directory.GetCurrentDirectory() + @"\data\" + TexBTenfile.Text + ".txt", true);
+                        sWriter.WriteLine(datet.ToString() + " " + s);
+                        sWriter.Flush();
+                        sWriter.Close();
+                    }
+                     
+                    //Datetime_Now = DateTime.Now;
+                    //Load_Data(datet.ToString());
+                }
+                catch { }
+            }
+            else
+            {
+                serialPort.ReadExisting();
+            }
+          
+        }
+
+        private delegate void R_Data(string s);
+        private void Load_Data(string s)
+        {
+            /*if(TexBNhan.InvokeRequired)
+            {
+                R_Data sd = new R_Data(Load_Data);
+                //TexBNhan.Invoke(sd, new object[] { s });
+            }
+            else
+            {
+                //if(TexBNhan.Text.Length>100000)
+                //{
+                //    TexBNhan.Text = "";
+                //}
+                //TexBNhan.Text = s; //TexBNhan.Text +
+
+            }*/
+        }
+
+        private void BtGui_Click(object sender, EventArgs e)
+        {
+            int millisecond = DateTime.Now.Millisecond;
+            Datetime_Now = DateTime.Now;
+            //TexBGui.Text =datetime.ToString() +" : "+ millisecond.ToString();
+            //timer1.Enabled = false;
+        }
+
+        double i = 0.000;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            millisecond = DateTime.Now.Millisecond - millisecond;
+            double datet = ((TimeSpan)(DateTime.Now - Datetime_Now)).TotalMilliseconds; // tính khoảng thời gian
+            //datetime = DateTime.Now;
+            datet = Math.Round(datet / 1000, 3);
+            //Load_Data(datet.ToString());
+            //i += 0.1;
+            draw(10, 13, 15, 17, 19, 20, datet);
+          
+        }
+
+        // vẽ đồ thị ;
+        private void draw(double Data_AX,double Data_AY, double Data_AZ, double Data_Roll_x, double Data_Pitch_y, double Data_Yaw_z, double time)
+        {
+            LineItem Curve_AX = ZedGraphGiaToc.GraphPane.CurveList[0] as LineItem; // khoi tao biến edit line 0 
+            IPointListEdit List_AX = Curve_AX.Points as IPointListEdit;
+            List_AX.Add(time, Data_AX);                                            // thêm điểm
+
+            LineItem Curve_AY = ZedGraphGiaToc.GraphPane.CurveList[1] as LineItem;
+            IPointListEdit List_AY = Curve_AY.Points as IPointListEdit;
+            List_AY.Add(time, Data_AY);
+
+            LineItem Curve_AZ = ZedGraphGiaToc.GraphPane.CurveList[2] as LineItem;
+            IPointListEdit List_AZ = Curve_AZ.Points as IPointListEdit;
+            List_AZ.Add(time, Data_AZ);
+           
+            //List_Roll_x List_Pitch_y  List_Yaw_z
+
+            LineItem Curve_Roll_x = ZedGraphGiaToc.GraphPane.CurveList[3] as LineItem;
+            IPointListEdit List_Roll_x = Curve_Roll_x.Points as IPointListEdit;
+            List_Roll_x.Add(time, Data_Roll_x);
+
+            LineItem Curve_Pitch_y = ZedGraphGiaToc.GraphPane.CurveList[4] as LineItem;
+            IPointListEdit List_Pitch_y = Curve_Pitch_y.Points as IPointListEdit;
+            List_Pitch_y.Add(time, Data_Pitch_y);
+
+            LineItem Curve_Yaw_z = ZedGraphGiaToc.GraphPane.CurveList[5] as LineItem;
+            IPointListEdit List_Yaw_z = Curve_Yaw_z.Points as IPointListEdit;
+            List_Yaw_z.Add(time, Data_Yaw_z);
+
+
+            Scale xScale = ZedGraphGiaToc.GraphPane.XAxis.Scale;
+            if (time > xScale.Max - xScale.MajorStep)
+            {
+                xScale.Max = time + xScale.MajorStep;
+                xScale.Min = xScale.Max - 10.0; // giá trị độ giãn
+            }
+
+            //Display sensor data
+            axisChangeZedGraph(ZedGraphGiaToc);
+
+        }
+        delegate void axisChangeZedGraphCallBack(ZedGraphControl zg);
+        private void axisChangeZedGraph(ZedGraphControl zg)
+        {
+            if (zg.InvokeRequired)
+            {
+                axisChangeZedGraphCallBack ad = new axisChangeZedGraphCallBack(axisChangeZedGraph);
+                zg.Invoke(ad, new object[] { zg });
+            }
+            else
+            {
+                //zg.AxisChange();
+                zg.Invalidate();
+                zg.Refresh();
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                serialPort.Close();
+
+            }
+            catch
+            {  }
+        }
+
+        private void BtLoad_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog loadfile= new OpenFileDialog();
+            loadfile.InitialDirectory = Application.StartupPath;
+            loadfile.CheckPathExists = true;
+            loadfile.DefaultExt = "txt";
+            loadfile.CheckFileExists = true;
+            loadfile.Filter = "text (*.txt)|*.txt|Tất cả file (*.*)|*.*";
+            loadfile.FilterIndex = 2;
+            loadfile.RestoreDirectory = true;
+            if (loadfile.ShowDialog() == DialogResult.OK)
+            {
+                TexBTenfile.Text = System.IO.Path.GetFileName(loadfile.FileName);
+                ZedGraphGiaToc.IsShowPointValues = true;
+                //textBox1.Text = loadfile.FileName.ToString();
+                ClearZedGraph();
+                StreamReader sRead = new StreamReader(loadfile.FileName.ToString(), true);
+                string dataread = sRead.ReadLine();
+                //textBox1.Text = dataread;
+                do{
+                    dataread = sRead.ReadLine();
+                    try
+                    {
+                        string[] araystring = dataread.Split(' ');
+                        draw(Convert.ToDouble(araystring[1]), Convert.ToDouble(araystring[2]), Convert.ToDouble(araystring[3]), Convert.ToDouble(araystring[4]), Convert.ToDouble(araystring[5]), Convert.ToDouble(araystring[6]), Convert.ToDouble(araystring[0])); // ve do thi 6 diem 
+                    }
+                    catch { }
+                }
+                while (dataread != null);
+            }
+        }
+        private void ClearZedGraph()
+        {
+            ZedGraphGiaToc.GraphPane.CurveList.Clear(); // Xóa đường
+            ZedGraphGiaToc.GraphPane.GraphObjList.Clear(); // Xóa đối tượng
+
+            ZedGraphGiaToc.AxisChange();
+            ZedGraphGiaToc.Invalidate();
+
+            GraphPane myPane = ZedGraphGiaToc.GraphPane;
+            //myPane.Title.Text = "Đồ thị dữ liệu theo thời gian";
+            //myPane.XAxis.Title.Text = "Thời gian (s)";
+            //myPane.YAxis.Title.Text = "Dữ liệu";
+
+            //RollingPointPairList list = new RollingPointPairList(60000);
+            //LineItem curve = myPane.AddCurve("Dữ liệu", list, Color.Red, SymbolType.None);
+
+            //myPane.XAxis.Scale.Min = 0;
+            //myPane.XAxis.Scale.Max = 30;
+           // myPane.XAxis.Scale.MinorStep = 1;
+            //myPane.XAxis.Scale.MajorStep = 5;
+            //myPane.YAxis.Scale.Min = -100;
+            //myPane.YAxis.Scale.Max = 100;
+
+            //ZedGraphGiaToc.AxisChange();
+
+
+
+            myPane.Title.Text = " Gia tốc, góc quay";
+            myPane.XAxis.Title.Text = "Thời Gian, giây";
+            myPane.YAxis.Title.Text = "Gia tốc,vân tốc góc, góc quay";
+            List_Ax.Clear();
+            List_Ay.Clear();
+            List_Az.Clear();
+            List_Roll_x.Clear();
+            List_Pitch_y.Clear();
+            List_Yaw_z.Clear();
+            LineItem Ax = myPane.AddCurve("Roll x", List_Ax, Color.Blue, SymbolType.None);
+            LineItem Ay = myPane.AddCurve("Pitch y", List_Ay, Color.Red, SymbolType.None);
+            LineItem Az = myPane.AddCurve("Yaw z", List_Az, Color.Yellow, SymbolType.None);
+            LineItem Roll_x = myPane.AddCurve("Ax", List_Roll_x, Color.Violet, SymbolType.None);
+            LineItem Pitch_y = myPane.AddCurve("Ay", List_Pitch_y, Color.Tomato, SymbolType.None);
+            LineItem Yaw_z = myPane.AddCurve("Az", List_Yaw_z, Color.Turquoise, SymbolType.None);
+
+            myPane.XAxis.Scale.Min = 0;
+            myPane.XAxis.Scale.Max = 10;
+            myPane.YAxis.Scale.Min = -3;
+            myPane.YAxis.Scale.Max = 3;
+            myPane.XAxis.Scale.MinorStep = 1;
+            myPane.XAxis.Scale.MajorStep = 1;
+            ZedGraphGiaToc.AxisChange();
+        }
+
+        
+       
+    }
+}
